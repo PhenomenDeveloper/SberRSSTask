7cc513b2401b7fa757bc00c4d23d6e5766770184
Kolesnikov Lev <phenomendeveloper@gmail.com>
2020-11-02 23:41:17 +0300

    add start project

---
+++ SberRSSTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SberRSSTask
+//
+//  Created by Lev Kolesnikov on 02.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
